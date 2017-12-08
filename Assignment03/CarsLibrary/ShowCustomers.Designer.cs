afe7eb59e3f06cf644162d38587d4d00701f14c9
atlc <andrewlloydcartwright+a@gmail.com>
2017-12-08 16:40:53 -0600

    Added in data library for Cars DB (with Customer table for Assignment03)

--- Assignment03/CarsLibrary/ShowCustomers.Designer.cs
+++ Assignment03/CarsLibrary/ShowCustomers.Designer.cs
@@ -1,6 +1,6 @@
-﻿namespace Assignment01
+﻿namespace CarsLibrary
 {
-    partial class Customers
+    partial class ShowCustomers
     {
         /// <summary>
         /// Required designer variable.
@@ -28,35 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Customers));
-            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(319, 335);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Close";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // Customers
+            // ShowCustomers
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(404, 367);
-            this.Controls.Add(this.button1);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Name = "Customers";
-            this.Text = "Customers";
+            this.ClientSize = new System.Drawing.Size(493, 308);
+            this.Name = "ShowCustomers";
+            this.Text = "ShowCustomers";
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
     }
 }
