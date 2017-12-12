5eb5d24e5e1d0658e84f3752225fd5cb74c4b878
atlc <andrewlloydcartwright+a@gmail.com>
2017-12-11 21:10:01 -0600

    Duplicated Assignment04 into Assignment07 for ASPX implementation

---
+++ Assignment07/Assignment01/Vehicles.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assignment01
+{
+    public partial class Vehicles : Form
+    {
+        public Vehicles()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private CarsLibrary.CarsEntities dbcontext = new CarsLibrary.CarsEntities();
+
+        private void Vehicles_Load(object sender, EventArgs e)
+        {
+            dbcontext.Vehicles.Load();
+            vehicleBindingSource.DataSource = dbcontext.Vehicles.Local;
+        }
+
+        private void sizeComboBox_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+            int index = sizeComboBox.SelectedIndex;
+            switch (index)
+            {
+                case 0:
+                    vehicleBindingSource.DataSource = dbcontext.Vehicles.Local.Where(Vehicles => Vehicles.CarSize1.Size == "Economy");
+                    break;
+                case 1:
+                    vehicleBindingSource.DataSource = dbcontext.Vehicles.Local.Where(Vehicles => Vehicles.CarSize1.Size == "Luxury");
+                    break;
+                case 2:
+                    vehicleBindingSource.DataSource = dbcontext.Vehicles.Local.Where(Vehicles => Vehicles.CarSize1.Size == "Mid-size");
+                    break;
+            }
+            vehicleBindingSource.MoveFirst();
+        }
+    }
+}
