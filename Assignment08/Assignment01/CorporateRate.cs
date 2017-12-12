12da217ae0760008924c3dc1ca36141be4185253
Andrew Cartwright <andrewlloydcartwright+atlc@gmail.com>
2017-12-12 12:38:37 -0600

    Cloned Assignment07 to initialize Assignment08

---
+++ Assignment08/Assignment01/CorporateRate.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment01
+{
+    public enum Discount : int
+    {
+        Corporate = 1,
+        Insurance
+    }
+
+    // Corporate Rate Class
+    class CorporateRate : RentalRate
+    {
+        private int DiscountRateInteger;
+        const Decimal CORPORATE_DISCOUNT_Decimal = 0.05M;
+        const Decimal INSURANCE_DISCOUNT_Decimal = 0.10M;   // Original value was 0.01M, corrected to 0.10M
+
+        public CorporateRate(int BeginnerOdometerInteger, int EndingOdometerInteger, int CarSizeInteger, int DaysInteger, int DiscountInteger)
+            : base (BeginnerOdometerInteger, EndingOdometerInteger, CarSizeInteger, DaysInteger)
+        {
+            DiscountRateInteger = DiscountInteger;
+            FindAmountDue();
+        }
+
+        // overrides base form method FindAmountDue()
+        private new Decimal FindAmountDue()
+        {
+
+            // Determine the amount due.
+            Decimal SubTotalDecimal, DiscountedDailyRateDecimal;
+
+            switch ((Discount)DiscountRateInteger)  // uses enum Discount defined above
+            {
+                case Discount.Corporate:
+
+                    // do math to determine AmountDueDecimal
+                    // Corporate accounts waive the mileage rate and have a 5 percent discount.
+                    
+                    SubTotalDecimal = ((DailyRateDecimal - (DailyRateDecimal * CORPORATE_DISCOUNT_Decimal)) * NumberDaysInteger);
+                    AmountDueDecimal = SubTotalDecimal;
+                    break;
+
+                case Discount.Insurance:
+
+                    // Insurance accounts have a 10 percent discount on the daily rate.
+                    // do math here
+                    // There is no mileage charge if the average of the miles
+                    // does not exceed an average of 100 miles per day rented.
+                    DiscountedDailyRateDecimal = ((DailyRateDecimal - (DailyRateDecimal * INSURANCE_DISCOUNT_Decimal)) * NumberDaysInteger);
+                    // if statement to decide the correct calculation
+                    if (AvgDailyMilesDecimal <= MAX_MILES_Integer)
+                        AmountDueDecimal = DiscountedDailyRateDecimal;
+                    else
+                    {
+                        SubTotalDecimal = DiscountedDailyRateDecimal + (NumberMilesInteger * MileageRateDecimal);
+                        AmountDueDecimal = SubTotalDecimal;
+                    }
+                    break;
+            }
+
+            return AmountDueDecimal;
+        }
+
+
+    }
+}
---
+++ Assignment08/RentalCarService/AddCustomer.aspx.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace RentalCarService
+{
+    public partial class AddCustomer : System.Web.UI.Page
+    {
+        private RentalCarService.CarsEntities dbcontext = new RentalCarService.CarsEntities();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            UnobtrusiveValidationMode = System.Web.UI.UnobtrusiveValidationMode.None;
+
+            if (IsPostBack)
+            {
+                Validate();
+            }
+        }
+
+        protected void ClearButton_Click(object sender, EventArgs e)
+        {
+            LastNameTextBox.Text = "";
+            FirstNameTextBox.Text = "";
+            PhoneNumberTextBox.Text = "";
+            StreetAddressTextBox.Text = "";
+            CityTextBox.Text = "";
+            StateTextBox.Text = "";
+            ZIPCodeTextBox.Text = "";
+            DriversLicenseTextBox.Text = "";
+        }
+
+        protected void SubmitButton_Click(object sender, EventArgs e)
+        {
+            if (IsValid)
+            {
+                var newCust = new Customer
+                {
+                    FName = FirstNameTextBox.Text,
+                    LName = LastNameTextBox.Text,
+                    Phone = PhoneNumberTextBox.Text,
+                    Street = StreetAddressTextBox.Text,
+                    City = CityTextBox.Text,
+                    State = StateTextBox.Text,
+                    Zip = ZIPCodeTextBox.Text,
+                    Drivers_License = DriversLicenseTextBox.Text
+                };
+
+                dbcontext.Customers.Add(newCust);
+                // Didn't know if we needed persistent state beyond session or not, so wrote it to db anyway
+                dbcontext.Customers.SaveChanges();
+            }
+        }
+    }
+}
